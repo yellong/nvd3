ad3c7a030a04359dd5fec70f479174526d17b9d2
yellong <yellongweek@gmail.com>
2013-09-24 15:25:31 +0800

    update radarChart

--- src/models/radar.js
+++ src/models/radar.js
@@ -1,5 +1,6 @@
 nv.models.radarChart = function() {
 
+    //public var
     var metrics = null,
         margin  = {top: 40, right: 40, bottom: 40, left: 40},
         width  = 500,
@@ -20,6 +21,9 @@ nv.models.radarChart = function() {
         transitionDuration = 500,
         pointRadius = 3,
         labelOffset = 10,
+        noData = "No Data Available.",
+        sortFunc = function(a,b){ return a.weight - b.weight - Math.random()*4 },
+        sort = true,
         dispatch = d3.dispatch('tooltipShow','tooltipHide');
 
     var showTooltip = function(e, offsetElement) {
@@ -29,6 +33,7 @@ nv.models.radarChart = function() {
         nv.tooltip.show([left, top], content, null, null, null);
     };
 
+    //calc and get Metrics in data
     var getMetrics = function(data){
         var extents={},metrics=[],extentAll=[];
 
@@ -60,13 +65,38 @@ nv.models.radarChart = function() {
                 availableHeight = height - margin.top - margin.bottom,
                 container = d3.select(this);
 
+            chart.update = function() { container.transition().duration(transitionDuration).call(chart); };
+            chart.container = this;
+
             radius = Math.min(availableWidth, availableHeight) / 2;
 
             metrics = getMetrics(data);
 
-            metrics = metrics
-                .sort(function(a,b){return a.weight- b.weight})
-                .map(function (metric, i) {
+            //no Data
+            if (!data || !data.length || !data.filter(function(d) { return d.values.length }).length || metrics.length<3) {
+                var noDataText = container.selectAll('.nv-noData').data([ noData ]);
+                container.selectAll('.nv-radarChart').remove();
+
+                noDataText.enter().append('text')
+                    .attr('class', 'nvd3 nv-noData')
+                    .attr('dy', '-.7em')
+                    .style('text-anchor', 'middle');
+
+                noDataText
+                    .attr('x', margin.left + availableWidth / 2)
+                    .attr('y', margin.top + availableHeight / 2)
+                    .text(function(d) { return d });
+
+                return chart;
+            } else {
+                container.selectAll('.nv-noData').remove();
+            }
+
+            if(sort){
+                metrics = metrics.sort(sortFunc);
+            }
+
+            metrics = metrics.map(function (metric, i) {
 
                 metric.scale = d3.scale.linear()
                     .range([ 0, radius ])
@@ -75,6 +105,7 @@ nv.models.radarChart = function() {
                 metric.angle = 2 * Math.PI * i / metrics.length - Math.PI / 2;
 
                 return metric;
+
             });
 
             data = data.map(function (d) {
@@ -112,125 +143,45 @@ nv.models.radarChart = function() {
                 .attr('height', availableHeight);
 
             var backgroundEnter = gEnter.append("g").attr('class','background');
+            var background  = g.select('.background');
 
-            chart.drawBackground( backgroundEnter );
+            chart.drawBackground( background );
 
-            gEnter.selectAll('.axis')
-                .data(function (d) { return d; })
+            var axisesEnter = gEnter.append("g").attr('class','axises');
+            var axises  = g.select('.axises');
+
+            axises.selectAll('.axis').remove();
+            axises.selectAll('.axis').data(function (d) { return d; })
                 .enter()
                 .append('g')
-                .attr('class', 'axis')
-                .each(function (d) {
-
+                .attr('class', 'axis').each(function (d) {
                     d3.select(this).append('g')
                         .attr('class', d.metric + ' axisScale')
                         .attr('transform', 'rotate(' + (d.angle * 180 / Math.PI) + ')')
                         .call( d3.svg.axis().scale(d.scale).tickSize(0) );
-
                     chart.drawAxisLabel(d3.select(this));
                 });
 
-            gEnter.selectAll('.radar-area')
-                .data(data)
-                .enter()
+
+
+            var radarArea = g.selectAll('.radar-area').data(data);
+
+            radarArea.enter()
                 .append('g')
-                .attr('class', function (d, i) { return 'nv-groups radar-area radar-area' + i; })
-                .attr('fill',function(d,i){ return color(d,i) })
-                .attr('stroke',function(d,i){return color(d,i) })
+                .attr('class', function (d, i) { return 'nv-groups radar-area radar-area' + i; });
+
+            radarArea.exit().remove();
+
+            g.selectAll('.radar-area').transition().duration(transitionDuration)
+//                .attr('class', function (d, i) { return 'nv-groups radar-area radar-area' + i; })
+                .style('fill', function(d,i){ return color(d,i) })
+                .style('stroke',function(d,i){return color(d,i) })
                 .each(function () {
                     chart.drawArea(d3.select(this));
-                })
+                });
 
         });
     }
-
-    chart.transitionDuration = function(_){
-        if (!arguments.length) {
-            return transitionDuration;
-        }
-        transitionDuration = _;
-        return chart;
-    }
-
-    chart.x = function(_){
-        if (!arguments.length) {
-            return getX;
-        }
-        getX = _;
-        return chart;
-    }
-
-    chart.y = function(_){
-        if (!arguments.length) {
-            return getY;
-        }
-        getY = _;
-        return chart;
-    }
-
-    chart.tooltips = function(_){
-        if (!arguments.length) {
-            return tooltips;
-        }
-        tooltips = _;
-        return chart;
-    }
-
-    chart.metrics = function(_){
-        if (!arguments.length) {
-            return metrics;
-        }
-        metrics = _;
-        return chart;
-    }
-
-    chart.color = function(_){
-        if (!arguments.length) {
-            return color;
-        }
-        color = _;
-        return chart;
-    }
-
-    chart.width = function(_){
-        if (!arguments.length) {
-            return width;
-        }
-        width = _;
-        return chart;
-    }
-
-    chart.height = function(_){
-        if (!arguments.length) {
-            return height;
-        }
-        height = _;
-        return chart;
-    }
-
-    chart.radius = function (_) {
-        if (!arguments.length) {
-            return radius;
-        }
-        radius = _;
-        return chart;
-    };
-
-    chart.margin = function (_) {
-        if (!arguments.length) {
-            return margin;
-        }
-        margin = _;
-        return chart;
-    };
-
-    chart.pointRadius = function (_) {
-        if (!arguments.length) {
-            return pointRadius;
-        }
-        pointRadius = _;
-        return chart;
-    };
 
     chart.drawBackground = function (selection) {
 
@@ -245,7 +196,6 @@ nv.models.radarChart = function() {
 
         while(r){
             var points = metrics.map(function(m,i){
-                console.log(m.label);
                 return {
                     loop: loop,
                     x : r * Math.cos(m.angle),
@@ -259,14 +209,16 @@ nv.models.radarChart = function() {
             r -= step;
         }
 
+        selection.selectAll('g').remove();
+
         selection.data([data]).selectAll('g').data(function(d){return d}).enter().append('g').selectAll('line')
-            .data(function(d){return d;}).enter()
-            .append('line').attr('stroke',function(d){ return d.loop===0?'#666':'#ccc'}).attr('stroke-width',function(d){ return d.loop===0?2:1 }).call(lineXY);
+            .data(function(d){return d;}).enter().append('line').attr('stroke',function(d){ return d.loop===0?'#666':'#ccc'}).attr('stroke-width',function(d){ return d.loop===0?2:1 }).call(lineXY);
 
     };
 
     chart.drawAxisLabel = function (selection) {
 
+        selection.selectAll('text').remove();
         selection.append('text')
             .text(function (d) { return d.label; })
             .attr('class', 'axisLabel')
@@ -287,15 +239,12 @@ nv.models.radarChart = function() {
 
     chart.drawArea = function (selection) {
 
-        selection.append('path')
+        var path = selection.selectAll('path')
+            .data(function(d){return [d];});
+
+        path.enter()
+            .append('path')
             .attr('class', 'radar-path')
-            .attr('d', function (d) {
-                return d3.svg.line()
-                    .x(function (d) { return d.x; })
-                    .y(function (d) { return d.y; })
-                    .interpolate('linear-closed')
-                    .call(this, d.points);
-            })
             .on('mousemove',function(d,i){
                 d3.select(this).classed('hover',true);
                 var e = {
@@ -325,7 +274,8 @@ nv.models.radarChart = function() {
                     }
                 )();
                 dispatch.tooltipShow(e);
-            }).on('mouseout',function(d,i){
+            })
+            .on('mouseout',function(d,i){
                 d3.select(this).classed('hover',false);
                 var e = {
                     label: d.label,
@@ -337,20 +287,29 @@ nv.models.radarChart = function() {
                 nv.tooltip.cleanup();
                 dispatch.tooltipHide(e);
             });
-        ;
 
-        selection.selectAll('.radar-point')
+        path.exit().remove();
+
+        selection.selectAll('path').transition()
+            .duration(transitionDuration)
+            .attr('d', function (d) {
+                return d3.svg.line()
+                    .x(function (d) { return d.x; })
+                    .y(function (d) { return d.y; })
+                    .interpolate('linear-closed')
+                    .call(this, d.points);
+            });
+
+        var point = selection.selectAll('.radar-point')
             .data(function (d) {
                 return d.points;
-            })
-            .enter()
+            });
+
+        point.exit().remove();
+
+        point.enter()
             .append('circle')
-            .attr('cx', function (d) { return d.x; })
-            .attr('cy', function (d) { return d.y; })
-            .attr('r', pointRadius)
             .attr('class', 'radar-point nv-point')
-//            .attr('fill',function(d,i){return color(d,i)})
-            .attr('stroke',function(d,i){return color(d,i)})
             .on('mouseover',function(d,i){
                 d3.select(this).classed('hover',true);
                 var e = {
@@ -362,7 +321,8 @@ nv.models.radarChart = function() {
                 }
                 if(tooltips)showTooltip(e,this.parentNode);
                 dispatch.tooltipShow(e);
-            }).on('mouseout',function(d,i){
+            })
+            .on('mouseout',function(d,i){
                 d3.select(this).classed('hover',false);
                 var e = {
                     label: d.label,
@@ -374,6 +334,116 @@ nv.models.radarChart = function() {
                 nv.tooltip.cleanup();
                 dispatch.tooltipHide(e);
             });
+
+        selection.selectAll('.radar-point').transition().duration(transitionDuration)
+            .attr('cx', function (d) { return d.x; })
+            .attr('cy', function (d) { return d.y; })
+            .attr('r', pointRadius)
+            .attr('stroke',function(d,i){return color(d,i)});
+    };
+
+    chart.noData = function(_){
+        if (!arguments.length) {
+            return noData;
+        }
+        noData = _;
+        return chart;
+    };
+
+    chart.sort = function(_){
+        if(!arguments.length){
+            return sort;
+        }
+        sort = _;
+        return chart;
+    };
+
+    chart.transitionDuration = function(_){
+        if (!arguments.length) {
+            return transitionDuration;
+        }
+        transitionDuration = _;
+        return chart;
+    };
+
+    chart.x = function(_){
+        if (!arguments.length) {
+            return getX;
+        }
+        getX = _;
+        return chart;
+    };
+
+    chart.y = function(_){
+        if (!arguments.length) {
+            return getY;
+        }
+        getY = _;
+        return chart;
+    };
+
+    chart.tooltips = function(_){
+        if (!arguments.length) {
+            return tooltips;
+        }
+        tooltips = _;
+        return chart;
+    };
+
+    chart.metrics = function(_){
+        if (!arguments.length) {
+            return metrics;
+        }
+        metrics = _;
+        return chart;
+    };
+
+    chart.color = function(_){
+        if (!arguments.length) {
+            return color;
+        }
+        color = _;
+        return chart;
+    };
+
+    chart.width = function(_){
+        if (!arguments.length) {
+            return width;
+        }
+        width = _;
+        return chart;
+    };
+
+    chart.height = function(_){
+        if (!arguments.length) {
+            return height;
+        }
+        height = _;
+        return chart;
+    };
+
+    chart.radius = function (_) {
+        if (!arguments.length) {
+            return radius;
+        }
+        radius = _;
+        return chart;
+    };
+
+    chart.margin = function (_) {
+        if (!arguments.length) {
+            return margin;
+        }
+        margin = _;
+        return chart;
+    };
+
+    chart.pointRadius = function (_) {
+        if (!arguments.length) {
+            return pointRadius;
+        }
+        pointRadius = _;
+        return chart;
     };
 
     return chart;
